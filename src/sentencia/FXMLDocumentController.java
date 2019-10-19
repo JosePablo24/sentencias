668b28d3719b4a1fc8dabba15e68c6acdada7eac
JosePablo24 <173198@ids.upchiapas.edu.mx>
2019-10-19 18:56:36 -0500

    asd

--- src/sentencia/FXMLDocumentController.java
+++ src/sentencia/FXMLDocumentController.java
@@ -11,6 +11,7 @@ import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.control.Label;
+import javafx.scene.control.TextArea;
 
 /**
  *
@@ -18,11 +19,24 @@ import javafx.scene.control.Label;
  */
 public class FXMLDocumentController implements Initializable {
     
-    @FXML
-    private Label label;
+    @FXML private Label label;
+    @FXML private TextArea area;
+    
     
     @FXML
     private void handleButtonAction(ActionEvent event) {
+        System.out.println("You clicked me!");
+        label.setText("Hello World!");
+    }
+    
+    @FXML
+    private void limpiar(ActionEvent event) {
+        area.setText("");
+        
+    }
+    
+    @FXML
+    private void descomponer(ActionEvent event) {
         System.out.println("You clicked me!");
         label.setText("Hello World!");
     }
